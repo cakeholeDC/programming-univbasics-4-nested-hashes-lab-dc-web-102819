1d33ea18588ad5bb7077552d2130faed9484896f
Kyle Cole <kylepcole@gmail.com>
2019-10-15 23:45:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,8 +88,7 @@ def adding_matz
 
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Front-end wizardry"
-  programmer_hash[:yukihiro_matsumoto][:languages] = [
-    "javascript", "php"]
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["javascript", "php"]
     
 end
 
